752f39a92313ad880d144735eaf4b7561d6e3776
flosacca <flosacca@gmail.com>
2025-09-20 00:13:43 +0800

    refine tests

--- test/z85_test.rb
+++ test/z85_test.rb
@@ -2,22 +2,30 @@ require 'z85'
 
 describe Z85 do
   it 'preserves encoding' do
-    assert_invertible "\x86\x4F\xD2\x6F\xB5\x59\xF7\x5B".b, 'HelloWorld'.b
-    assert_invertible "\0\0\0\0", '00000'
+    assert_bidirectional '', ''
+    assert_bidirectional ''.b, ''.b
+    assert_bidirectional "\0\0\0\0", '00000'
+    assert_bidirectional "\x86\x4F\xD2\x6F\xB5\x59\xF7\x5B".b, 'HelloWorld'.b
   end
 
   it 'handles lengths not divisible by four' do
-    assert_invertible "\0", '00'
-    assert_invertible "\0\0", '000'
-    assert_invertible "\0\0\0", '0000'
-    assert_invertible 'a', 've'
-    assert_invertible 'aa', 'vpo'
-    assert_invertible 'aaa', 'vprN'
+    assert_bidirectional "\0", '00'
+    assert_bidirectional "\0\0", '000'
+    assert_bidirectional "\0\0\0", '0000'
+    assert_bidirectional 'a', 've'
+    assert_bidirectional 'aa', 'vpo'
+    assert_bidirectional 'aaa', 'vprN'
   end
 
   it 'handles multibyte characters' do
-    assert_invertible "\u304a\u3084\u3059\u307f", '<ai9FF}SfnND5>k'
-    assert_invertible "\u3053\u3093\u306b\u3061\u306f", '<aiAOF%kkCTk!=8<ajx'
+    assert_bidirectional "\u304a\u3084\u3059\u307f", '<ai9FF}SfnND5>k'
+    assert_bidirectional "\u3053\u3093\u306b\u3061\u306f", '<aiAOF%kkCTk!=8<ajx'
+  end
+
+  it 'handles random bytes' do
+    10.times do
+      assert_invertible Random.bytes(rand(1000))
+    end
   end
 
   describe 'the decoder' do
@@ -45,8 +53,12 @@ describe Z85 do
     end
   end
 
-  def assert_invertible(binary, ascii)
+  def assert_bidirectional(binary, ascii)
     assert_equal ascii, Z85.encode(binary)
     assert_equal binary, Z85.decode(ascii)
+  end
+
+  def assert_invertible(binary)
+    assert_equal binary, Z85.decode(Z85.encode(binary))
   end
 end
